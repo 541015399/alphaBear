2592aef962d38108d30f242a2def22c19e3223a5
lupinyin <lupinyin@bogon>
2017-12-01 19:37:30 +0800

    4000

---
+++ src/main/java/sf/alphaBear/agent/GreedyAstarBearV1936.java
@@ -0,0 +1,123 @@
+package sf.alphaBear.agent;
+
+import java.awt.datatransfer.FlavorTable;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.lucene.analysis.fi.FinnishAnalyzer;
+import org.neo4j.graphdb.Path;
+
+import scala.reflect.internal.Symbols.ClassSymbol;
+import scala.reflect.internal.Trees.New;
+import sf.alphaBear.Config;
+import sf.alphaBear.MoveDecision;
+import sf.alphaBear.Point;
+import sf.alphaBear.agent.algo.AstarAlgoByNeo4j;
+import sf.alphaBear.agent.algo.SchedulePath;
+import sf.alphaBear.agent.db.GridStateDb;
+import sf.alphaBear.httpio.pojo.AI;
+import sf.alphaBear.httpio.pojo.Job;
+
+public class GreedyAstarBearV1936 extends BearTemplate {
+	GridStateDb db;
+	AstarAlgoByNeo4j astarAlgo;
+
+	JobProfit curJobProfit ;
+	int curStep = 0;
+	SchedulePath schedulePath ;
+	List<Point> walkHis ;
+	
+	public GreedyAstarBearV1936(BearContext context) {
+		super(context);
+		
+		walkHis = new ArrayList<Point>();
+		db = new GridStateDb(Config.MAX_X, Config.MAX_Y, context.getWalls());
+		astarAlgo = new AstarAlgoByNeo4j(db);
+	}
+	
+	private JobProfit reSchedule(List<Job> jobs, AI ai) {
+		
+		List<JobProfit> jobProfits = jobs.stream().map(j->{
+			SchedulePath schedulePath = astarAlgo.findPath(ai.getX(), ai.getY(), j.getX(), j.getY());
+			int steps = schedulePath.maxSteps() + 1;
+			JobDetail detail = context.getDetail(j);
+			int cost = detail==null? steps : detail.predictReward(steps);
+			int profit = j.getValue() - cost;
+			float avgProfit = profit/(float)steps;
+			return new JobProfit(j, profit, avgProfit, schedulePath);
+		})
+			.filter(o->o.getProfit()>0)
+			.collect(Collectors.toList());
+		
+		List<JobProfit> newList = new ArrayList<>();
+		int range = 1;
+		while(newList.size()==0) {
+			final int fRange = range;
+			newList = jobProfits.stream().filter(o->o.getPath().maxSteps()<=fRange).collect(Collectors.toList());
+			range ++;
+		}
+		jobProfits = newList;
+		
+		jobProfits.sort(new Comparator<JobProfit>() {
+			@Override
+			public int compare(JobProfit o1, JobProfit o2) {
+				// return o2.getProfit() - o1.getProfit();
+				Float f2 = o2.getAvgProfit();
+				Float f1 = o1.getAvgProfit();
+				return f2.compareTo(f1);
+			}
+		});
+		
+		SchedulePath path = jobProfits.get(0).getPath();
+		Point target = path.get(path.maxSteps());
+		System.out.println("reschedule - x=" + target.getX() + ",y=" + target.getY());
+		return jobProfits.get(0);
+	}
+
+	public MoveDecision myDecision() {
+		AI ai = context.getAI();
+		walkHis.add(new Point(ai.getX(), ai.getY()));
+		List<Job> jobs = context.getJobs();
+		//初始化， 或者走到头了
+		if (curJobProfit==null || schedulePath==null || schedulePath.maxSteps()<curStep) {
+			curJobProfit = null;
+			schedulePath = null;
+			curStep = 0;
+			if (jobs!=null && jobs.size()>0) {
+				curJobProfit = reSchedule(jobs, ai);
+				schedulePath = curJobProfit.getPath();
+				
+				Point target = schedulePath.get(curStep ++);
+				return calDirection(ai, target);
+			}else {
+				return randomWalk(ai);
+			}
+		}else {
+			// 正常走，不管如何，对比一下
+			JobProfit tryJob = reSchedule(jobs, ai);
+			//if (tryJob.getProfit() > curJobProfit.getProfit()) {
+			if (tryJob.getAvgProfit() > curJobProfit.getAvgProfit()) {
+				System.out.println("reschedule gain -" + (tryJob.getProfit() - curJobProfit.getProfit()));
+				curJobProfit = tryJob;
+				schedulePath = curJobProfit.getPath();
+				curStep = 0;
+				
+				Point target = schedulePath.get(curStep ++);
+				return calDirection(ai, target);
+			}else {
+				Point target = schedulePath.get(curStep ++);
+				return calDirection(ai, target);
+			}
+		}
+		
+		
+	}
+
+}
